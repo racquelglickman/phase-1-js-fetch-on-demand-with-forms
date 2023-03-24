a678cb55b071f34b0917003f89ff791aabe85c49
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-24 12:51:52 -0400

    complete fetch with forms lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const inputFormText = document.querySelector("input#searchByID").value;
+
+        fetch(`http://localhost:3000/movies/${inputFormText}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                // console.log('all done')
+                const title = document.querySelector("section#movieDetails h4");
+                const summary = document.querySelector("section#movieDetails p");
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+             })  
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
